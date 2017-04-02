1374fe5d0bba381f662abde34492c91e9203f548
Vasily Shelkov <vasilydshelkov@gmail.com>
2017-04-02 15:26:33 +0100

    Initial attempt at circle 2.0

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git remote add heroku-staging https://git.heroku.com/regency-wealth-crm-staging.git
+git remote add heroku-master https://git.heroku.com/regency-wealth-crm-staging.git
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
