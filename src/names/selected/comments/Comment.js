16c19dd4f8facd75057f562c5c6b578fd7665dfc
Vasily Shelkov <vasilydshelkov@gmail.com>
2017-04-14 21:37:09 +0100

    hide ability to edit/delete comments of other users

--- src/names/selected/comments/Comment.js
+++ src/names/selected/comments/Comment.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
@@ -8,11 +7,9 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import DeleteName from 'material-ui/svg-icons/action/delete';
 import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
-import PrivateIcon from 'material-ui/svg-icons/action/visibility';
-import PublicIcon from 'material-ui/svg-icons/social/people';
 import { grey400, red500, cyan500 } from 'material-ui/styles/colors';
 
-import EditCommentForm from './EditCommentForm';
+import CommentManipulator from './CommentManipulator';
 
 export default class Comment extends Component {
   state = ({ editingComment: false });
@@ -22,16 +19,8 @@ export default class Comment extends Component {
 
   render() {
     const {
-      userId,
-      id,
-      subject,
-      text,
-      visibility,
-      created_at,
-      updated_at,
-      commentUser,
-      editComment,
-      deleteComment
+      userId, subject, commentUser, deleteComment,
+      ...propsForManipulator
     } = this.props;
 
     const menuIconButton = (
@@ -45,6 +34,7 @@ export default class Comment extends Component {
         <div className="Comment__header">
           <div className="Comment__header__container">
             <Avatar>{commentUser.firstName[0].toUpperCase()}</Avatar>
+
             <div className="Comment__header__container__text">
               <div className="Comment__header__container__text__username">
                 {userId === commentUser.id ? 'Me' : `${commentUser.firstName} ${commentUser.lastName}`}
@@ -54,51 +44,30 @@ export default class Comment extends Component {
               </div>
             </div>
           </div>
-          <IconMenu
-            iconButtonElement={menuIconButton}
-            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-          >
-            <MenuItem onTouchTap={this.showEditComment} leftIcon={<EditIcon color={cyan500} />}>
-              Edit
-            </MenuItem>
-            <MenuItem onTouchTap={deleteComment} leftIcon={<DeleteName color={red500} />}>
-              Delete
-            </MenuItem>
-          </IconMenu>
+          {
+            userId === commentUser.id && (
+              <IconMenu
+                iconButtonElement={menuIconButton}
+                anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+              >
+                <MenuItem onTouchTap={this.showEditComment} leftIcon={<EditIcon color={cyan500} />}>
+                  Edit
+                </MenuItem>
+                <MenuItem onTouchTap={deleteComment} leftIcon={<DeleteName color={red500} />}>
+                  Delete
+                </MenuItem>
+              </IconMenu>
+            )
+          }
         </div>
-        {
-          this.state.editingComment ?
-            <EditCommentForm
-              initialValues={{ text, visibility }}
-              userId={userId}
-              commentId={id}
-              hideCommentForm={this.hideEditComment}
-            />
-          :
-            <div>
-              <div className="Comment__content">{text}</div>
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <div className="Comment__time">
-                  <div>{moment(updated_at).fromNow()}</div>
-                  &nbsp;·
-                  {created_at !== updated_at ? <div>Edited</div> : ''}
-                </div>
-                {
-                  visibility === 'private' ?
-                    <IconButton iconStyle={{ cursor: 'default' }} tooltip="Private">
-                      <PrivateIcon />
-                    </IconButton>
-                  :
-                    <IconButton iconStyle={{ cursor: 'default' }} tooltip="Public">
-                      <PublicIcon />
-                    </IconButton>
-                }
-              </div>
-            </div>
-        }
+        <CommentManipulator
+          userId={userId}
+          editingComment={this.state.editingComment}
+          hideEditComment={this.hideEditComment}
+          {...propsForManipulator}
+        />
       </div>
     );
   }
 }
-
