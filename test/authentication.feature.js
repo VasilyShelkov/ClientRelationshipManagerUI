ad5737a296144e686b0d8c826f0b847b58d06bc0
Vasily <vasilydshelkov@gmail.com>
2017-05-06 16:10:42 +0100

    Fixed authentication feature

--- test/authentication.feature.js
+++ test/authentication.feature.js
@@ -1,12 +1,12 @@
 Feature('Authentication', { retries: 3 });
 
-Scenario('User is redirected to login when not logged in', I => {
+Scenario('User is redirected to login when not logged in', (I) => {
   I.amOnPage('/account/profile');
   I.seeInCurrentUrl('/login');
   I.seeElement('.Login__form');
 });
 
-Scenario('User is taken to their profile after logging in', I => {
+Scenario('User is taken to their profile after logging in', (I) => {
   I.login();
   I.waitForElement('.Profile');
   I.seeInCurrentUrl('/account/profile');
