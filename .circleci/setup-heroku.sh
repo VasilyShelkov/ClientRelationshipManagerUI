9acba1585bbf477993178466b3a7fb41eeab20c0
Vasily Shelkov <vasilydshelkov@gmail.com>
2017-05-07 11:37:49 +0100

    hopefully fixing master build

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git remote add heroku-staging https://git.heroku.com/regency-wealth-crm-staging.git
-git remote add heroku-master https://git.heroku.com/regency-wealth-crm-staging.git
+git remote add heroku-master https://git.heroku.com/regency-wealth-crm.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
