fb91d03ff3b2133eca8eea411c7e6e592a1077b7
Vasily Shelkov <vasilydshelkov@gmail.com>
2017-11-12 18:55:21 +0000

    upgrade dev dependencies

--- testSetup.js
+++ testSetup.js
@@ -1,25 +1,26 @@
-require('babel-polyfill');
+require("babel-polyfill");
 
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const sinon = require('sinon');
-const sinonTest = require('sinon-test');
-const sinonChai = require('sinon-chai');
-const shallow = require('enzyme').shallow;
-const getMuiTheme = require('material-ui/styles/getMuiTheme').default;
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const sinon = require("sinon");
+sinon.test = require("sinon-test")(sinon);
+const sinonChai = require("sinon-chai");
+const Enzyme = require("enzyme");
+const Adapter = require("enzyme-adapter-react-15");
+Enzyme.configure({ adapter: new Adapter() });
+const shallow = Enzyme.shallow;
+const getMuiTheme = require("material-ui/styles/getMuiTheme").default;
 
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
-sinon.test = sinonTest.configureTest(sinon);
-
-const jsdom = require('jsdom').jsdom;
-const document = jsdom('');
+const jsdom = require("jsdom").jsdom;
+const document = jsdom("");
 
 global.document = document;
 global.window = document.defaultView;
 global.navigator = {
-  userAgent: 'node.js'
+  userAgent: "node.js"
 };
 
 global.expect = chai.expect;
