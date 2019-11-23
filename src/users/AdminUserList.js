d18a80d804f012479b3e6fe26d3fb674cfd0fd95
Vasily Shelkov <v.shelkov@iwoca.co.uk>
2019-11-23 20:57:45 +0000

    finished the new user page

--- src/users/AdminUserList.js
+++ src/users/AdminUserList.js
@@ -1,100 +1,75 @@
 import React from 'react';
-import _ from 'lodash';
+import { connect } from 'react-redux';
 
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import Avatar from '@material-ui/core/Avatar';
 
-import PersonAddIcon from 'material-ui/svg-icons/social/person-add';
-import { green500 } from 'material-ui/styles/colors';
-import toMaterialStyle from 'material-color-hash';
-import LoadingSpinner from '../shared/LoadingSpinner';
+import { changeShownUserProfile } from '../profile/profileActions';
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
-    maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-export default ({ currentUserId, loading, users, value, onChange }) => {
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-
+const AdminUserList = ({ currentUserId, loading, users, showUserProfile }) => {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
-      <List
-        component="nav"
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            <span id="totalUserCount">{users.length}</span> Users
-          </ListSubheader>
-        }
-      >
-        <ListItem
-          id="createNewUser"
-          button
-          selected={false}
-          value={JSON.stringify({
-            // changeME
-            newRoute: '/account/users/add',
-            currentUserId,
-            userIdToShow: currentUserId,
-          })}
-          onClick={event => {}}
-        >
-          <ListItemIcon>
-            <Avatar icon={<PersonAddIcon />} backgroundColor={green500} />
-          </ListItemIcon>
-          <ListItemText primary="Create New User" />
-        </ListItem>
-
-        <Divider />
-
-        {users.length ? (
-          users.map(user => (
-            <ListItem
-              key={`profile-${user.id}`}
-              button
-              value={JSON.stringify({
-                // CHANGEME
-                newRoute: `/account/users/${_.camelCase(
-                  `${user.firstName} ${user.lastName}`,
-                )}/profile`,
-                currentUserId,
-                userIdToShow: user.id,
-              })}
-              selected={false}
-              onClick={event => {}}
-            >
-              <ListItemIcon>
-                <Avatar
-                  style={toMaterialStyle(
-                    user.firstName[0].toUpperCase() +
-                      user.lastName[0].toUpperCase(),
-                  )}
-                >
-                  {user.firstName[0].toUpperCase()}
-                  {user.lastName[0].toUpperCase()}
-                </Avatar>
-              </ListItemIcon>
-              <ListItemText primary={`${user.firstName} ${user.lastName}`} />
-            </ListItem>
-          ))
-        ) : (
-          <ListItem disabled>
-            <ListItemText primary="There are no other users..." />
-          </ListItem>
+      <Autocomplete
+        style={{ width: '100%' }}
+        getOptionLabel={user => `${user.firstName} ${user.lastName}`}
+        options={users}
+        loading={loading}
+        clearOnEscape={true}
+        loadingText={'Loading existing users...'}
+        onChange={(event, user) => {
+          showUserProfile(currentUserId, user ? user.id : currentUserId);
+        }}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label={`Search user...`}
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
         )}
-      </List>
+      />
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  currentUserId: state.account.id,
+});
+
+const mapDispatchToProps = dispatch => ({
+  showUserProfile: (currentUserId, userIdToShow) =>
+    dispatch(
+      changeShownUserProfile({
+        currentUserId,
+        userIdToShow,
+        isNewUser: false,
+      }),
+    ),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AdminUserList);
--- src/users/UsersPage.js
+++ src/users/UsersPage.js
@@ -1,56 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PeopleIcon from '@material-ui/icons/People';
 
 import AdminUserListWithData from './AdminUserListWithData';
-import { changeSideBarState } from '../authentication/accountActions';
-import { changeShownUserProfile } from '../profile/profileActions';
-import { resetScrollPosition } from '../names/nameActions';
+import AddUserForm from './AddUserForm';
+import ProfileWithData from '../profile/ProfileWithData';
 
-const UsersPage = ({
-  currentUserId,
-  currentPage,
-  profileUserId,
-  handleRouteChange,
-}) => {
-  const selectedValue = JSON.stringify({
-    newRoute: currentPage,
-    currentUserId,
-    userIdToShow: profileUserId,
-  });
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: '10px',
+  },
+}));
+
+const UsersPage = ({ currentUserId, profileUserId }) => {
+  const styles = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
-    <AdminUserListWithData
-      currentUserId={currentUserId}
-      value={selectedValue}
-      onChange={handleRouteChange}
-    />
+    <>
+      <AppBar position="relative" color="default" className={styles.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Edit Existing users" icon={<PeopleIcon />} />
+          <Tab label="Create New User" icon={<PersonAddIcon />} />
+        </Tabs>
+      </AppBar>
+      {value === 0 ? (
+        <>
+          <AdminUserListWithData currentUserId={currentUserId} />
+          {currentUserId !== profileUserId ? <ProfileWithData /> : null}
+        </>
+      ) : null}
+      {value === 1 ? <AddUserForm /> : null}
+    </>
   );
 };
 
 const mapStateToProps = state => ({
-  currentPage: state.routing.location.pathname,
   currentUserId: state.account.id,
   profileUserId: state.profile.id,
 });
 
-const mapDispatchToProps = dispatch => ({
-  handleRouteChange: (event, linkValue) => {
-    const { newRoute, currentUserId, userIdToShow } = JSON.parse(linkValue);
-    dispatch(changeSideBarState(false));
-    dispatch(push(newRoute));
-    dispatch(
-      changeShownUserProfile({
-        currentUserId,
-        userIdToShow,
-        isNewUser: false,
-      }),
-    );
-    dispatch(resetScrollPosition());
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(UsersPage);
+export default connect(mapStateToProps)(UsersPage);
