5180e823411238e09e2197e4f1c0463baf86f6b2
Vasily Shelkov <v.shelkov@iwoca.co.uk>
2019-11-24 21:31:14 +0000

    refactor ShowProfile to use the latest version of Martial-ui

--- src/profile/details/ShowProfile.js
+++ src/profile/details/ShowProfile.js
@@ -2,30 +2,30 @@ import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 
-import Paper from 'material-ui/Paper';
-import { List, ListItem } from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import RaisedButton from 'material-ui/RaisedButton';
-import Chip from 'material-ui/Chip';
-import Avatar from 'material-ui/Avatar';
-
-import EditIcon from 'material-ui/svg-icons/content/create';
-import ProfileIcon from 'material-ui/svg-icons/social/person';
-import PhoneIcon from 'material-ui/svg-icons/communication/phone';
-import EmailIcon from 'material-ui/svg-icons/communication/email';
-import SecurityIcon from 'material-ui/svg-icons/hardware/security';
-import UpdatedIcon from 'material-ui/svg-icons/action/update';
-import SuccessfulUpdateIcon from 'material-ui/svg-icons/action/done';
-import SuccessfullyCreatedIcon from 'material-ui/svg-icons/action/check-circle';
-import {
-  cyan500,
-  green900,
-  green700,
-  lightGreen300,
-} from 'material-ui/styles/colors';
-import { ProtectedIcon } from '../../app/icons';
 import toMaterialStyle from 'material-color-hash';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 
+import EditIcon from '@material-ui/icons/Edit';
+import ProfileIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
+import SecurityIcon from '@material-ui/icons/Security';
+import UpdateIcon from '@material-ui/icons/Update';
+import SuccessfulUpdateIcon from '@material-ui/icons/Check';
+import SuccessfullyCreatedIcon from '@material-ui/icons/CheckCircle';
+import { green700 } from 'material-ui/styles/colors';
+import { makeStyles } from '@material-ui/styles';
+
+import { ProtectedIcon } from '../../app/icons';
 import Notification from '../../shared/Notification';
 import EditPassword from './EditPassword';
 import {
@@ -36,6 +36,35 @@ import {
 } from '../profileActions';
 import { EDIT_IN_PROGRESS } from '../profileReducer';
 
+const useStyles = (firstName, lastName) =>
+  makeStyles(theme => {
+    return {
+      root: {
+        paddingBottom: '10px',
+        margin: '20px 0px',
+      },
+      profileIcon: {
+        height: '100px',
+        width: '100px',
+        position: 'relative',
+        display: 'inline-block',
+        left: '20px',
+      },
+      profileAvatar: {
+        ...toMaterialStyle(
+          `${firstName[0].toUpperCase()}${lastName[0].toUpperCase()}`,
+        ),
+        position: 'relative',
+        top: '-60px',
+        right: '50px',
+        display: 'inline-flex',
+      },
+      avatar: {
+        color: 'white',
+        backgroundColor: theme.palette.primary.main,
+      },
+    };
+  });
 export const ShowProfile = ({
   id,
   firstName,
@@ -51,131 +80,124 @@ export const ShowProfile = ({
   onEditProfilePassword,
   onCancelEditProfilePassword,
   onRemoveNotification,
-}) => (
-  <Paper zDepth={2} style={{ paddingBottom: '10px', margin: '20px 0px' }}>
-    <div style={{ textAlign: 'center', padding: '0px 10px' }}>
-      <div style={{ height: '85px' }}>
-        <ProfileIcon
-          style={{
-            height: '100px',
-            width: '100px',
-            position: 'relative',
-            left: '20px',
-          }}
-          color={cyan500}
-        />
-        <Avatar
-          style={{
-            ...toMaterialStyle(
-              `${firstName[0].toUpperCase()}${lastName[0].toUpperCase()}`,
-            ),
-            position: 'relative',
-            top: '-60px',
-            right: '50px',
-          }}
-        >
-          {firstName[0].toUpperCase()}
-          {lastName[0].toUpperCase()}
-        </Avatar>
-      </div>
-      <h2>
-        {firstName} {lastName}
-      </h2>
-      <Notification
-        message={newUserNotification}
-        zDepth={3}
-        backgroundColor={green700}
-        icon={
-          <SuccessfullyCreatedIcon
-            className="Form__notification__icon"
-            style={{ color: 'white' }}
-          />
-        }
-      />
-    </div>
-
-    <List>
-      <Divider />
-
-      <ListItem
-        leftAvatar={
-          <Avatar icon={<ProtectedIcon />} backgroundColor={cyan500} />
-        }
-        primaryText={`Limit: ${protectedNamesLimit}`}
-        disabled
-      />
-      <ListItem
-        leftAvatar={<Avatar icon={<EmailIcon />} backgroundColor={cyan500} />}
-        primaryText={email}
-        disabled
-      />
-      <ListItem
-        leftAvatar={<Avatar icon={<PhoneIcon />} backgroundColor={cyan500} />}
-        primaryText={phone}
-        disabled
-      />
-
-      <Divider />
-
-      {editingPassword ? (
-        <EditPassword
-          userId={id}
-          editInProgress={editingPassword === EDIT_IN_PROGRESS}
-          handleCancelEditProfilePassword={onCancelEditProfilePassword}
-        />
-      ) : (
-        <ListItem
-          data-testid="resetPassword"
-          leftAvatar={
-            <Avatar icon={<SecurityIcon />} backgroundColor={cyan500} />
-          }
-          rightIcon={<EditIcon />}
-          primaryText="Password"
-          secondaryText="**********"
-          onClick={onEditProfilePassword}
-        />
-      )}
-
-      <Divider />
-    </List>
-
-    <div className="Profile__meta-info">
-      <div className="row justify-content-center">
-        {editSuccessProfileNotification && (
-          <Chip
-            id="editProfileSuccess"
-            style={{ marginBottom: '10px' }}
-            backgroundColor={lightGreen300}
-            onRequestDelete={onRemoveNotification}
-            onTouchTap={onRemoveNotification}
-          >
-            <Avatar
-              size={32}
-              icon={<SuccessfulUpdateIcon />}
-              color={lightGreen300}
-              backgroundColor={green900}
+}) => {
+  const styles = useStyles(firstName, lastName)();
+  return (
+    <Paper zDepth={2} className={styles.root}>
+      <div style={{ textAlign: 'center', padding: '0px 10px' }}>
+        <div style={{ height: '85px' }}>
+          <ProfileIcon color="primary" className={styles.profileIcon} />
+          <Avatar className={styles.profileAvatar}>
+            {firstName[0].toUpperCase()}
+            {lastName[0].toUpperCase()}
+          </Avatar>
+        </div>
+        <h2>
+          {firstName} {lastName}
+        </h2>
+        <Notification
+          message={newUserNotification}
+          zDepth={3}
+          backgroundColor={green700}
+          icon={
+            <SuccessfullyCreatedIcon
+              className="Form__notification__icon"
+              style={{ color: 'white' }}
             />
-            {editSuccessProfileNotification}
-          </Chip>
+          }
+        />
+      </div>
+
+      <List>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar className={styles.avatar}>
+              <ProtectedIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={`Limit: ${protectedNamesLimit}`} />
+        </ListItem>
+
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar className={styles.avatar}>
+              <EmailIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={email} />
+        </ListItem>
+
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar className={styles.avatar}>
+              <PhoneIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={phone} />
+        </ListItem>
+
+        <Divider />
+
+        {editingPassword ? (
+          <EditPassword
+            userId={id}
+            editInProgress={editingPassword === EDIT_IN_PROGRESS}
+            handleCancelEditProfilePassword={onCancelEditProfilePassword}
+          />
+        ) : (
+          <ListItem
+            data-testid="resetPassword"
+            button
+            onClick={onEditProfilePassword}
+          >
+            <ListItemAvatar>
+              <Avatar className={styles.avatar}>
+                <SecurityIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Password" secondary="**********" />
+            <ListItemSecondaryAction>
+              <EditIcon />
+            </ListItemSecondaryAction>
+          </ListItem>
         )}
+        <Divider />
+      </List>
+
+      <div className="Profile__meta-info">
+        <div className="row justify-content-center">
+          {editSuccessProfileNotification && (
+            <Chip
+              id="editProfileSuccess"
+              style={{ marginBottom: '10px' }}
+              color="secondary"
+              icon={<SuccessfulUpdateIcon />}
+              label={editSuccessProfileNotification}
+              onDelete={onRemoveNotification}
+            />
+          )}
+        </div>
+        <div className="row justify-content-center">
+          <Chip
+            color="primary"
+            icon={<UpdateIcon />}
+            label={`Last Updated: ${moment(updated_at).fromNow()}`}
+          />
+        </div>
       </div>
-      <div className="row justify-content-center">
-        <Chip>
-          <Avatar size={32} icon={<UpdatedIcon />} backgroundColor={cyan500} />
-          <strong>Last Updated</strong>: {moment(updated_at).fromNow()}
-        </Chip>
+      <div className="Profile__cta row justify-content-center">
+        <Button
+          color="primary"
+          variant="contained"
+          startIcon={<EditIcon />}
+          onClick={onEditProfile}
+        >
+          Edit profile
+        </Button>
       </div>
-    </div>
-    <div className="Profile__cta row justify-content-center">
-      <RaisedButton
-        primary
-        label="Edit Profile"
-        icon={<EditIcon />}
-        onClick={onEditProfile}
-      />
-    </div>
-  </Paper>
-);
+    </Paper>
+  );
+};
 
 const mapStateToProps = state => ({
   editSuccessProfileNotification: state.profile.notification.profile,
